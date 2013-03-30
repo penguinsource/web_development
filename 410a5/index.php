cbba80dbeb18187f547fc426178269ff891e43d8
penguinsource <mihaioprescu5@yahoo.com>
2013-03-30 12:49:26 -0600

    a5..

--- 410a5/index.php
+++ 410a5/index.php
@@ -11,8 +11,8 @@
 <link rel="stylesheet" type="text/css" href="design.css">
 
 <script type="text/javascript">
-	
-	function sendRequest(type, input){
+var itemIDselected = null;
+	function sendRequest(type, input, idSelected){
 		//alert("type of req:" + type + ", value:" + document.getElementById("name").value);
 		var xmlhttp;
 		if (window.XMLHttpRequest){	// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -25,29 +25,70 @@
 		xmlhttp.onreadystatechange=function(){
 		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
 			{
-			//document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-			alert("response: "+xmlhttp.responseText);
+				//document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+				
+				//alert("response: "+xmlhttp.responseText);
+				
+				object = xmlhttp.responseText;
+				obj = JSON.parse(object);
+				alert(obj[0].desc);
+				if (obj[0].desc == null || obj[0].desc == ''){	// check to see which call is being received
+					contentDiv = document.getElementById("content");
+					for (i = 0; i < obj.length;i++){
+						contentDiv.innerHTML += "<div><button onClick=\"sendRequest('GET', 'description'," + obj[i].id + ")\" id='btnID"+ obj[i].id +
+												"'>Get Desc</button> "+ obj[i].name + "</br><div id='item"+obj[i].id+"'></div></div>";
+					}
+				} else {
+					//itemDiv = document.getElementById("content");
+					//itemDiv.innerHTML="blah blah";
+					
+				}
+				//obj = JSON.stringify(object)
+				//var haha = eval("("+object+")");
+				
+				//alert("length:"+obj.length);
+
 			}
 		}
 		
 		var url = "products";
 		if (type == "GET"){
 			var params = "";
-			if (input == "description"){
+			if (input == "description"){		// for call: 	  GET /api/items/1
 				//params += "id="+"1";	// description for item with id .. document.getElement...
-				params += "/1";
-			}
-			//alert("params: " + url+"/"+params);
+				params += "/"+idSelected;
+				itemIDselected = idSelected;
+			} 									// else for call: GET /api/items
+			//alert("GET call url: " + url+"/"+params);
 			xmlhttp.open("GET",url+params,true);
 			xmlhttp.send();
-		} else if (type == "POST"){
-			var nameVal = document.getElementById("name").value;
-			var priceVal = document.getElementById("price").value;
-			var descVal = document.getElementById("description").value;
-			var params = "name="+nameVal+"&price="+priceVal+"&desc="+descVal; 
+		} else if (type == "POST"){				// for call: 	  POST /api/items
+			var nameVal = "'" + document.getElementById("name").value + "'";
+			var priceVal = "'" + document.getElementById("price").value + "'";
+			var descVal = "'" + document.getElementById("description").value + "'";
 			
+			if ((nameVal == null) || nameVal == ""){
+				alert("Did ya forget the name of your item ?");
+				document.getElementById("name").focus();
+				return;
+			} else if ((priceVal == null) || priceVal == ""){
+				alert("Did ya forget the price of your item ?");
+				document.getElementById("price").focus();
+				return;
+			} else if ((descVal == null) || descVal == ""){
+				alert("Did ya forget the description of your item ?");
+				document.getElementById("description").focus();
+				return;
+			}
+			var jsonObj= {
+			"name":nameVal,
+			"price":priceVal, 
+			"desc":descVal};
+			var blah = JSON.stringify(jsonObj);
+			//var params = "name="+nameVal+"&price="+priceVal+"&desc="+descVal; 
+			var params = "data="+encodeURIComponent(blah);
 			xmlhttp.open("POST", url, true);
-			//alert("params:" + params);
+			//alert("POST call url: " + url + ", params: " + params);
 			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
 			xmlhttp.setRequestHeader("Content-length", params.length); 
 			xmlhttp.setRequestHeader("Connection", "close"); 
@@ -55,7 +96,6 @@
 			xmlhttp.send(params);
 		}
 	}
-
 </script>
 
 </head>
@@ -84,6 +124,10 @@
 		<hr>
 		<button onClick="sendRequest('GET', 'catalogue')"> Get Catalogue </button>
 		<button onClick="sendRequest('GET', 'description')"> Get Description </button>
+		
+		<div name="content" id="content">
+			
+		</div>
 		<!-- <form action="products.php" method="get">
 		<button>Get Catalogue</button>
 		</form> -->
--- 410a5/products.php
+++ 410a5/products.php
@@ -1,23 +1,79 @@
 <?php
 
-$n = 0;
 
-echo " ; request method is : " . $_SERVER['REQUEST_METHOD'] . " ; ";
 
-if (isset($_GET["id"])){
-	//echo "DATA RECEEEIVED => id=" . $_GET["id"];
-	echo "; returning description for id=" . $_GET["id"] . "; n=".$n;
-} else if (isset($_POST["name"])){
-	echo "; adding item with name=" . $_POST["name"]. "; n=".$n;
-} else {
-	echo "; wat" . "; n=".$n;
+
+if ($_SERVER['REQUEST_METHOD'] == "GET"){
+	//echo "GET request";
+	if (isset($_GET["id"])){
+		//echo "DATA RECEEEIVED => id=" . $_GET["id"];
+		//echo "; returning description for id=" . $_GET["id"];
+		//getItem($_GET["id"]);
+		//getAllItems();
+		getItem($_GET["id"]);
+		//getAllItems();
+	} else {	// return JSON catalogue
+		getAllItems();
+	}
+	
+} else if ($_SERVER['REQUEST_METHOD'] == "POST"){	// post request
+	if (isset($_POST["data"])){						// check if data exists
+		$object = json_decode($_POST["data"]);		// decode json
+		addObject($object->{'name'}, $object->{'desc'}, $object->{'price'});
+	}
 }
-$fp = fopen('data.txt', 'r');
-while ($line = fgets($fp)){
-	echo " line $n: " . $line . "\n";
-	$n++;
+
+function getCount(){
+	$n = 0;
+	$fp = fopen('data.txt', 'r');
+	while ($line = fgets($fp)){
+		//echo " line $n: " . $line . "\n";
+		$n++;
+	}
+	return $n;
 }
 
+function addObject($name, $desc, $price){
+	$count = getCount();
+	$fpAdd = fopen('data.txt', 'a');
+	fwrite($fpAdd, $count . " " . $name . " " . $desc . " " . $price);
+	fwrite($fpAdd, PHP_EOL);	// this is a line break
+	fclose($fpAdd);
+}
+
+function getAllItems(){
+	$fp = fopen('data.txt', 'r');
+	$list = array();
+	while ($line = fgets($fp)){
+		$parts = explode(' ', $line);
+		$temp = array('id' => $parts[0], 'name' => $parts[1]);
+		array_push($list, $temp);
+	}
+	//print_r(json_encode($list));
+	echo (json_encode($list));
+}
+
+function getItem($id){
+	$existsFlag = false;
+	$list = array();
+	while ($line = fgets($fp)){
+		$parts = explode(' ', $line);
+		if ($parts[0] == $id){
+			$existsFlag = true;
+			$temp = array('desc' => $parts[2], "price" => $parts[3]);
+			array_push($list, $temp);
+		}
+		$temp = array('id' => $parts[0], 'name' => $parts[1]);
+		array_push($list, $temp);
+	}
+	//if ($existsFlag == true){
+		echo (json_encode($list));
+	//} else{
+	//	echo null;
+	//}
+}
+
+/*
 // COUNT IS n ..
 fclose($fp);
 $dude="wtfsss";
@@ -25,6 +81,7 @@ $fp2 = fopen('data.txt', 'a');
 fwrite($fp2, PHP_EOL);	// this is a line break
 fwrite($fp2, $dude);
 fclose($fp2);
+*/
 
 /*$filee = "3 a e re twet w";
 $fileee = "bbbb";
